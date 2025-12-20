79604f8156d52b606c2b31294bc734e70c875225
SymphonyIceAttack <1994370030@qq.com>
2025-12-20 19:43:33 +0800

    feat: finish robots

--- app/robots.ts
+++ app/robots.ts
@@ -8,7 +8,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: "*",
       allow: "/",
-      disallow: ["/admin", "/api", "/_next/"],
+      disallow: ["/admin", "/api", "/private", "/_next", "/assets", "/static"],
     },
     sitemap: `${baseUrl}/sitemap.xml`,
   };
